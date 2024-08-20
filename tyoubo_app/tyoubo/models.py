93e0f572b43c19c0b07ecb7d57246af61a2fc15b
molhot <sakata19991214@gmail.com>
2024-08-20 13:36:54 +0900

    migration done.make Model TyouboConfig, it have name price type

--- tyoubo_app/tyoubo/models.py
+++ tyoubo_app/tyoubo/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class TyouboConfig(models.Model):
+    item_type_list = [
+        ('option1', '家庭用品'),
+        ('option2', '非家庭用品'),
+    ]
+    registed_item_name = models.CharField('item name', max_length=50)
+    item_price = models.IntegerField('item price')
+    item_type = models.CharField(max_length=20, choices=item_type_list) #選択肢にできるらしい...？
+
+    def __str__(self):
+        return f'登録名：{self.registed_item_name} 値段：{self.item_price}'
